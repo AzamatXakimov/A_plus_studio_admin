c08c93768da81ba796f421d5ca37e4a44ae54421
Djasurbekovich <elmurodziyovuddinov7@gmail.com>
2023-07-04 13:00:56 +0500

    Elmurod => done

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -7,59 +7,69 @@ import { useDispatch } from "react-redux";
 import { addToken } from "../../redux/token/tokenActions";
 
 export const Login = () => {
-    const userNameRef = useRef()
-    const passwordRef = useRef()
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const userNameRef = useRef();
+  const passwordRef = useRef();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const hendelSubmit = () => {
-        // axios.post("http://localhost:5000/user/login", {
-        //     userName: userNameRef.current.value,
-        //     password: passwordRef.current.value,
-        // }).then(res => {
-        //     if(res.status === 201){
-        //         dispatch(addToken(res.data.token));
-        //         localStorage.setItem("token", res.data.token)
-        //         navigate("/")
-        //     }
-        // }).catch(error => console.log(error))
+  const hendelSubmit = () => {
+    // axios.post("http://localhost:5000/user/login", {
+    //     userName: userNameRef.current.value,
+    //     password: passwordRef.current.value,
+    // }).then(res => {
+    //     if(res.status === 201){
+    //         dispatch(addToken(res.data.token));
+    //         localStorage.setItem("token", res.data.token)
+    //         navigate("/")
+    //     }
+    // }).catch(error => console.log(error))
 
+    dispatch(addToken(userNameRef.current.value));
+    localStorage.setItem("token", userNameRef.current.value);
+    navigate("/");
+  };
+  return (
+    <>
+      <div className="login-wrapper">
+        <div className="login-inner">
+          <img className="login-img" src={LoginImg} alt="Login Image" />
 
+          <form
+            className="login-form"
+            onSubmit={(evt) => {
+              evt.preventDefault();
+              hendelSubmit();
+            }}>
+            <Link className="login-logo-ling" to="/">
+              <img src={LoginLogo} alt="Site logo" />
+            </Link>
 
-        dispatch(addToken(userNameRef.current.value));
-        localStorage.setItem("token", userNameRef.current.value)
-        navigate("/")
-    }
-    return <>
-        <div className="login-wrapper">
-            <div className="login-inner">
-                <img className="login-img" src={LoginImg} alt="Login Image" />
-
-                <form className="login-form" onSubmit={evt => {
-                    evt.preventDefault();
-                    hendelSubmit()
-                }}>
-                    <Link className="login-logo-ling" to="/">
-                        <img src={LoginLogo} alt="Site logo" />
-                    </Link>
-
-                    <label className="login-label">
-                        <span className="login-label-text">
-                            Username
-                        </span>
-                        <input className="login-input" type="text" placeholder="Enter your username" ref={userNameRef} required/>
-                    </label>
-                    <label className="login-label">
-                        <span className="login-label-text">
-                            Password
-                        </span>
-                        <input className="login-input" type="password" placeholder="Enter your password" ref={passwordRef} required/>
-                    </label>
-                    <button className="login-btn" type="submit">
-                        Login
-                    </button>   
-                </form>
-            </div>
+            <label className="login-label">
+              <span className="login-label-text">Username</span>
+              <input
+                className="login-input"
+                type="text"
+                placeholder="Enter your username"
+                ref={userNameRef}
+                required
+              />
+            </label>
+            <label className="login-label">
+              <span className="login-label-text">Password</span>
+              <input
+                className="login-input"
+                type="password"
+                placeholder="Enter your password"
+                ref={passwordRef}
+                required
+              />
+            </label>
+            <button className="login-btn" type="submit">
+              Login
+            </button>
+          </form>
         </div>
+      </div>
     </>
-}
+  );
+};
